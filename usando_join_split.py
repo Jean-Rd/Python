3a235582666372a9566a86215e92294ff5b47073
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-06 19:17:46 -0400

    usando_join_split

---
+++ usando_join_split.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 18:48:04 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+cadena = input('Ingrese una cadena con varios espacios: ')
+cadena = cadena.split()
+print(' '.join(cadena))
