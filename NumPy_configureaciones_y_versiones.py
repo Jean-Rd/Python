d453bad5b489ca716f2cc1f064fdbd2bdb222ba2
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-10 20:12:53 -0400

    Create NumPy_configureaciones_y_versiones.py

---
+++ NumPy_configureaciones_y_versiones.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 19:05:01 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+import numpy as np
+
+print(np.__version__)
+print()
+print(np.show_config())
+    
