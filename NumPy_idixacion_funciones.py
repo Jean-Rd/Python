8c22ca96a5dc661fd8f1028acddc262230f0e900
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-10 20:12:46 -0400

    Create NumPy_idixacion_funciones.py

---
+++ NumPy_idixacion_funciones.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 19:48:01 2020
+
+@author: DELL.E5430.SSD
+"""
+
+#Idenxacion de matrices
+import numpy as np
+'''
+aca veremos que una matriz se puede usar como una funcion por asi decirlo
+
+'''
+a = np.arange(12)**2
+
+i = np.array([1,1,3,4,5])
+print(i)
+print()
+print(a[i])
+
+j = np.array([[1,3],[2,4]])#matrix
+print(j)
+print()
+print(a[j])
+
