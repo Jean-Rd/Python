5bbcc8d142c89d18b9a9193686b1a02553d1cae3
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-12 00:59:05 -0400

    Create 05.funcion_edad.py

---
+++ 05.funcion_edad.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 23:48:51 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+def mayor_de_edad(edad):
+    return edad >= 18
+
+edad = int(input('Digite su edad: '))
+print(mayor_de_edad(edad))
