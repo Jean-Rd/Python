db6585982636ed87f48c783527eaf46d40216207
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-12 21:15:02 -0400

    Create 07.funcion_is_lower.py

---
+++ 07.funcion_is_lower.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 01:01:35 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+def cadena(bolean):
+    bolean = texto[0]
+    return (bolean == texto[0].lower())
+        
+texto = input('Ingrese el texto: ')
+print(cadena(texto))
