1eb0ee95cfddc9e94ceead926b4ee034d7aa4228
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-12 22:28:28 -0400

    Update 09.funciones_nuemro_perfecto.py

--- 09.funciones_nuemro_perfecto.py
+++ 09.funciones_nuemro_perfecto.py
@@ -4,7 +4,9 @@ Created on Tue May 12 21:38:45 2020
 
 @author: DELL.E5430.SSD
 """
+import datetime
 
+ahora = datetime.datetime.now()
 def es_perfecto(n):
     sumatorio = 0
     for i in range(1,n):
@@ -16,5 +18,7 @@ def es_perfecto(n):
 numero = int(input('Digite un numero --> '))
 if(es_perfecto(numero) == True):
     print(f'El numero {numero},  si es perfecto!!!')
+    print(ahora.strftime('%d/%m/%Y  %H:%M:%S'))
 else:
     print(f'El numero {numero}, no es perfecto.')
+    print(ahora.strftime('%d/%m/%Y  %H:%M:%S'))
