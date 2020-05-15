6fdabb7f4c4c6d7e88c0458b5d40b20433c5d1ed
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-15 18:58:09 -0400

    Create 29.funcion_menu.py

---
+++ 29.funcion_menu.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 17:36:50 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+def menu():
+    opcion = ''
+    while not('a' <= opcion <= 'c'):
+        print('\n\n\tCajero Automatico\n\ta) Ingresar Dinero\n\tb) Retirar Dinero\n\tc) Consultar saldo')
+        opcion = input('Ingrese una opcion: ')
+        if not(opcion >= 'a' and opcion <= 'b'):
+            print('Por favor ingrese una opcion valida.')
+    return opcion
+
+a = menu()
