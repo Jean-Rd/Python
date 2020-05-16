9e4f0e25b15d4a31e4118e4a85eff71f6508b434
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-16 11:36:32 -0400

    Create 38.funcion_tabla_amigos_V2.py

---
+++ 38.funcion_tabla_amigos_V2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 10:41:32 2020
+
+@author: DELL.E5430.SSD
+"""
+
+import datetime
+
+def numero_amigo(n):
+        suma = 0
+        for i in range(1,n):
+            if(n % i == 0):
+                suma += i
+        return suma
+
+def tabla_amigos(m):
+    c_amigos = []
+    n_numeros = []
+    amiguitos = []
+    for i in range(1,m+1):
+        posible_amigo = numero_amigo(i)
+        if(posible_amigo in range(1,m+1)):
+            if(numero_amigo(posible_amigo) == i and posible_amigo != i and i and posible_amigo not in amiguitos):
+                amiguitos.append(posible_amigo and i)
+                print('Los numeros:',i,'^',posible_amigo,', son amigos.')
+
+# Programa Principal
+ahora = datetime.datetime.now()
+
+numero = int(input('Digite un numero --> '))
+print(tabla_amigos(numero))
+print()
+print(ahora.strftime('%d/%m/%Y  %H:%M:%S'))
