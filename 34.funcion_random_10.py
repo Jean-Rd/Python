f398b54aa91e4ba881582049cc1152830ffa58a0
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-15 21:02:24 -0400

    Create 34.funcion_random_10.py

---
+++ 34.funcion_random_10.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 20:58:57 2020
+
+@author: DELL.E5430.SSD
+"""
+
+import random
+
+def numero_random_10():
+    return random.randint(-10,10)
+
+print(numero_random_10())
