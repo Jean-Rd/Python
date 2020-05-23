541f0fc748ad2baebc234d1e5d63b2c6e14802aa
Jean-Rd <60849217+Jean-Rd@users.noreply.github.com>
2020-05-23 10:06:50 -0400

    Create 52.pandas_read.py

---
+++ 52.pandas_read.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 10:01:08 2020
+
+@author: DELL.E5430.SSD
+"""
+
+
+import pandas as pd
+
+titanic = pd.read_csv('titanic.csv.csv')
+print(titanic)
